85e6d2fc73552d5f041904e0178dde9c3ae00c31
Dfria <jdfria@gmail.com>
2024-01-16 22:04:46 -0800

    Added data cleaning function

--- scrape.py
+++ scrape.py
@@ -10,6 +10,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 import time
+import os
 
 WAIT_TIME = 15
 CHROMEDRIVER_PATH = r'E:\notarapidbot\Scrapper\chromedriver.exe'
@@ -18,12 +19,13 @@ service = Service(executable_path=CHROMEDRIVER_PATH)
 
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
+chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-def get_book_data(driver, link):
-    # Navigate to list of books for certain genre, shown on the left index of toscrape page
-    csv_file = r'E:\bookscraper\bookdata\books.csv'
+RAW_CSV_FILE = r'E:\bookscraper\bookdata\books.csv'
 
+def get_book_data(driver, link):
+    print(f"Navigating to: {link}")
     driver.get(link)
 
     genre = driver.find_element(By.XPATH, '//*[@id="default"]/div/div/div/div/div[1]').text
@@ -57,7 +59,7 @@ def get_book_data(driver, link):
 
         data.append(book_data)
     new_data = pd.DataFrame(data)
-    new_data.to_csv(csv_file, mode='a', header=False, index=False)
+    new_data.to_csv(RAW_CSV_FILE, mode='a', header=False, index=False)
 
     driver.get(link)
     time.sleep(2)
@@ -72,16 +74,57 @@ def get_book_data(driver, link):
 
         if next_page_link:
             get_book_data(driver, next_page_link)
-    return genre
+
 
 def scrape_books(driver):
-    driver.get("https://books.toscrape.com/catalogue/category/books_1/index.html")
+    if os.path.exists(RAW_CSV_FILE):
+        print(f"The file '{RAW_CSV_FILE}' already exists.")
+    else:
+        driver.get("https://books.toscrape.com/catalogue/category/books_1/index.html")
 
-    genre_index = driver.find_elements(By.XPATH, "/html/body/div/div/div/aside/div[2]/ul/li/ul/li/a")
-    links = [genre.get_attribute('href') for genre in genre_index]
+        genre_index = driver.find_elements(By.XPATH, "/html/body/div/div/div/aside/div[2]/ul/li/ul/li/a")
+        links = [genre.get_attribute('href') for genre in genre_index]
 
-    for link in links:
-        genre = get_book_data(driver, link)
-        print("Done scraping " + genre + " books.")
+        for link in links:
+            get_book_data(driver, link)
+            print("Done scraping section.")
 
-scrape_books(driver)
+def clean_data():
+    df = pd.read_csv(RAW_CSV_FILE)
+
+    replace_rating = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
+
+    # Replace values based on the mapping
+    df['rating'] = df['rating'].replace(replace_rating)
+
+    df['price'] = pd.to_numeric(df['price'].str.replace('£', ''), errors='coerce')
+    df['price-with-tax'] = pd.to_numeric(df['price-with-tax'].str.replace('£', ''), errors='coerce')
+
+    # Hypothetical exchange rate
+    exchange_rate = 1.12  # 1 Euro = 1.12 US Dollars
+
+    # Convert 'price' and 'price-with-tax' columns to US dollars and round to 2 decimal places
+    df['price'] = (df['price'] * exchange_rate).round(2)
+    df['price-with-tax'] = (df['price-with-tax'] * exchange_rate).round(2)
+
+    df.rename(columns={'price_usd': 'price'}, inplace=True)
+    df.rename(columns={'price-with-tax-usd': 'price-with-tax'}, inplace=True)
+
+    df['availability'] = df['availability'].str.extract(r'\((\d+) available\)').astype(int)
+
+    cleaned_csv_file = r'E:\bookscraper\bookdata\books_cleaned.csv'
+
+    # Check if the file already exists
+    if os.path.exists(cleaned_csv_file):
+        print(f"The file '{cleaned_csv_file}' already exists.")
+    else:
+        # Export the DataFrame to a new CSV file
+        df.to_csv(cleaned_csv_file, index=False)
+        print(f"The DataFrame has been successfully exported to '{cleaned_csv_file}'.")
+
+def main():
+    scrape_books(driver)
+    clean_data()
+
+if __name__ == "__main__":
+    main()
