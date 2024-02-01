3661cbeea048446dd4a54b42d1f60e91eab541e4
Dfria <jdfria@gmail.com>
2024-02-01 15:15:05 -0800

    Added MySQL Connector to localhost

--- scrape.py
+++ scrape.py
@@ -8,12 +8,14 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+import mysql.connector
 import pandas as pd
 import time
+import csv
 import os
 
 WAIT_TIME = 15
-CHROMEDRIVER_PATH = r'E:\notarapidbot\Scrapper\chromedriver.exe'
+CHROMEDRIVER_PATH = r'chromedriver\chromedriver.exe'
 
 service = Service(executable_path=CHROMEDRIVER_PATH)
 
@@ -22,7 +24,7 @@ chrome_options.add_experimental_option('detach', True)
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-RAW_CSV_FILE = r'E:\bookscraper\bookdata\books.csv'
+RAW_CSV_FILE = r'bookdata\books.csv'
 
 def get_book_data(driver, link):
     print(f"Navigating to: {link}")
@@ -75,19 +77,34 @@ def get_book_data(driver, link):
         if next_page_link:
             get_book_data(driver, next_page_link)
 
-
 def scrape_books(driver):
     if os.path.exists(RAW_CSV_FILE):
-        print(f"The file '{RAW_CSV_FILE}' already exists.")
-    else:
-        driver.get("https://books.toscrape.com/catalogue/category/books_1/index.html")
+        df = pd.read_csv(RAW_CSV_FILE)
+        num_records = len(df)
 
-        genre_index = driver.find_elements(By.XPATH, "/html/body/div/div/div/aside/div[2]/ul/li/ul/li/a")
-        links = [genre.get_attribute('href') for genre in genre_index]
+        print(f"The file '{RAW_CSV_FILE}' already exists!")
+        if num_records > 999:
+            print("Data scraping is finished.")
+            return ""
+        else:
+            print("Number of records: " + num_records)
+            os.remove(RAW_CSV_FILE)
+    
+    headers = ['upc','title','genre','price','price-with-tax','rating','availability']
 
-        for link in links:
-            get_book_data(driver, link)
-            print("Done scraping section.")
+    with open(RAW_CSV_FILE, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(headers)
+
+    print(f'The CSV file has been successfully created at: {RAW_CSV_FILE}')
+
+    driver.get("https://books.toscrape.com/index.html")
+    genre_index = driver.find_elements(By.XPATH, "/html/body/div/div/div/aside/div[2]/ul/li/ul/li/a")
+    links = [genre.get_attribute('href') for genre in genre_index]
+
+    for link in links:
+        get_book_data(driver, link)
+        print("Done scraping section.")
 
 def clean_data():
     df = pd.read_csv(RAW_CSV_FILE)
@@ -99,10 +116,11 @@ def clean_data():
     # Replace values based on the mapping
     df['rating'] = df['rating'].replace(replace_rating)
 
+    # remove euro sign from price and price-with-tax columns
     df['price'] = pd.to_numeric(df['price'].str.replace('£', ''), errors='coerce')
     df['price-with-tax'] = pd.to_numeric(df['price-with-tax'].str.replace('£', ''), errors='coerce')
 
-    # Hypothetical exchange rate
+    # Convert Euros to USD
     exchange_rate = 1.12  # 1 Euro = 1.12 US Dollars
 
     # Convert 'price' and 'price-with-tax' columns to US dollars and round to 2 decimal places
@@ -114,7 +132,7 @@ def clean_data():
 
     df['availability'] = df['availability'].str.extract(r'\((\d+) available\)').astype(int)
 
-    cleaned_csv_file = r'E:\bookscraper\bookdata\books_cleaned.csv'
+    cleaned_csv_file = r'bookdata\books_cleaned.csv'
 
     # Check if the file already exists
     if os.path.exists(cleaned_csv_file):
@@ -124,9 +142,53 @@ def clean_data():
         df.to_csv(cleaned_csv_file, index=False)
         print(f"The DataFrame has been successfully exported to '{cleaned_csv_file}'.")
 
+def import_data():
+    db_config = {
+        "host": "localhost",
+        "port": 3306,
+        "user": "root",
+        "password": "password",
+    }
+
+    db_name = 'bookstoscrape'
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+    cursor.execute(f"USE {db_name}")
+
+    table_name = 'books'
+    create_table_query = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            upc VARCHAR(16) UNIQUE PRIMARY KEY,
+            title VARCHAR(255),
+            genre VARCHAR(64),
+            price DECIMAL(10, 2),
+            `price-with-tax` DECIMAL(10, 2),
+            rating TINYINT UNSIGNED,
+            availability TINYINT UNSIGNED
+        )
+    """
+    cursor.execute(create_table_query)
+
+    # Store cleaned data in DataFrame
+    file_path = r'bookdata\books_cleaned.csv'
+    df = pd.read_csv(file_path)
+
+    # Iterrate each record into local database
+    # Column names: upc,title,genre,price,price-with-tax,rating,availability
+
+    for index, row in df.iterrows():
+        query = f"INSERT IGNORE INTO {table_name} (upc, title, genre, price, `price-with-tax`, rating, availability) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = tuple(row)
+        cursor.execute(query, values)
+
+    connection.commit()
+    connection.close()
+
 def main():
     scrape_books(driver)
     clean_data()
+    import_data()
 
 if __name__ == "__main__":
     main()
